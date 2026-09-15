519c812d943b8b915a865563438ffc11c6987b98
Samiah Saud <samiahsaud6@gmail.com>
2026-09-15 11:07:30 +0000

    add regex support to find

--- user/find.c
+++ user/find.c
@@ -9,11 +9,49 @@ fmtname(char *path)
 {
   char *p;
 
-  // Find first character after last slash.
   for(p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
   return p;
+}
+
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do{
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
 
 void
@@ -51,7 +89,7 @@ find(char *path, char *target, char **cmdargv, int cmdargc)
     return;
   }
 
-  if(strcmp(fmtname(path), target) == 0){
+  if(match(target, fmtname(path))){
     if(cmdargv)
       run_exec(cmdargv, cmdargc, path);
     else
@@ -96,13 +134,13 @@ main(int argc, char *argv[])
   int i;
 
   if(argc < 3){
-    fprintf(2, "usage: find dir name [-exec cmd ...]\n");
+    fprintf(2, "usage: find dir name \n");
     exit(1);
   }
 
   if(argc > 3){
     if(strcmp(argv[3], "-exec") != 0){
-      fprintf(2, "usage: find dir name [-exec cmd ...]\n");
+      fprintf(2, "usage: find dir name \n");
       exit(1);
     }
     for(i = 4; i < argc; i++){
